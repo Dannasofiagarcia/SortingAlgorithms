2282417fd8604e60845c5c1bface6b4d0ffdb4e6
Danna García <dannasofiagarciatrujillo@hotmail.com>
2021-03-09 22:12:09 -0500

    Sorter class ready

---
+++ SortingAlgorithms/model/Sorter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAlgorithms.model
+{
+    class Sorter
+    {
+        private void quicksort(int[] vector, int primero, int ultimo)
+        {
+            int i, j, central;
+            double pivote;
+            central = (primero + ultimo) / 2;
+            pivote = vector[central];
+            i = primero;
+            j = ultimo;
+            do
+            {
+                while (vector[i] < pivote) i++;
+                while (vector[j] > pivote) j--;
+                if (i <= j)
+                {
+                    int temp;
+                    temp = vector[i];
+                    vector[i] = vector[j];
+                    vector[j] = temp;
+                    i++;
+                    j--;
+                }
+            } while (i <= j);
+
+            if (primero < j)
+            {
+                quicksort(vector, primero, j);
+            }
+            if (i < ultimo)
+            {
+                quicksort(vector, i, ultimo);
+            }
+        }
+    }
+}
