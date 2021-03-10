e787a70cce9b33543e7ee09650e5065eab6fc107
Camilo Escobar <48284856+chumbi27escobar3@users.noreply.github.com>
2021-03-09 23:47:37 -0500

    Added generating method randomized size and order

--- SortingAlgorithms/model/DataGenerator.cs
+++ SortingAlgorithms/model/DataGenerator.cs
@@ -26,6 +26,24 @@ namespace SortingAlgorithms.model
                 data = Conf02(count);
             }
 
+             //caso Generate(0, 3, count)
+            if ((size == 0) && (order == 0))
+            {
+                Conf03();
+            }
+
+            //caso Generate(0, 4, count)
+            if ((size == 0) && (order == 1))
+            {
+                Conf04();
+            }
+
+            //caso Generate(0, 5, count)
+            if ((size == 0) && (order == 2))
+            {
+                Conf05();
+            }
+
             return data;
         }
 
@@ -65,6 +83,51 @@ namespace SortingAlgorithms.model
             for (int i = 0; i < data.Length; i++)
             {
                 data[i] = r.Next(0, count + 1);
+            }
+
+            return data;
+        }
+
+        //Genera arreglo con tamaño Aleatorio, orden ascendente
+        private static int[] Conf03()
+        {
+             Random r = new Random();
+
+            int[] data = new int[r.Next(10^(r.Next(1,7))];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = i;
+            }
+
+            return data;
+        }
+
+        //Genera arreglo con tamaño Aleatorio, orden descendente
+        private static int[] Conf04()
+        {
+             Random r = new Random();
+
+            int[] data = new int[r.Next(10^(r.Next(1,7))];
+
+            for (int i = data.Length - 1; i >= 0; i--)
+            {
+                data[i] = i;
+            }
+
+            return data;
+        }
+
+        //Genera arreglo con tamaño Aleatorio, orden aleatorio
+        private static int[] Conf05()
+        {
+            Random r = new Random();
+
+            int[] data = new int[r.Next(10^(r.Next(1,7)))];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                 data[i] = r.Next(0,10000);
             }
 
             return data;
