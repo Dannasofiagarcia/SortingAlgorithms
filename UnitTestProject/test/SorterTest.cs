8f2bd6d28c637799e9d55eda0e5b294d45e82e12
Danna García <dannasofiagarciatrujillo@hotmail.com>
2021-03-12 18:20:04 -0500

    Test ready

--- UnitTestProject/test/SorterTest.cs
+++ UnitTestProject/test/SorterTest.cs
@@ -11,9 +11,26 @@ namespace UnitTestProject
 
         public void SetUp1()
         {
+            //Probando en orden descendente
             temp = new int[3] {3, 2, 1 };
             
         }
+
+        public void SetUp2()
+        {
+            //Probando en orden ascendente
+            temp = new int[3] { 1, 2, 3 };
+
+        }
+
+        public void SetUp3()
+        {
+            //Probando en orden aleatorio
+            temp = new int[3] { 1, 3, 2 };
+
+        }
+
+
 
         [TestMethod]
         public void TestMethod1()
@@ -23,6 +40,133 @@ namespace UnitTestProject
             temp = SortingAlgorithms.model.Sorter.sort(temp, 0);
 
             Assert.IsTrue(temp[0] == 1);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            SetUp1();
+            //Quick
+            temp = SortingAlgorithms.model.Sorter.sort(temp, 1);
+
+            Assert.IsTrue(temp[0] == 1);
+
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            SetUp1();
+            int ultimo = 3;
+            //Quick
+            temp = SortingAlgorithms.model.Sorter.sort(temp, 1);
+
+            Assert.IsTrue(temp[2] == ultimo);
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            SetUp1();
+            int ultimo = 3;
+            //Quick
+            temp = SortingAlgorithms.model.Sorter.sort(temp, 0);
+
+            Assert.IsTrue(temp[2] == ultimo);
+
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            SetUp2();
+            //Bubble
+            temp = SortingAlgorithms.model.Sorter.sort(temp, 0);
+
+            Assert.IsTrue(temp[0] == 1);
+
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            SetUp2();
+            //Quick
+            temp = SortingAlgorithms.model.Sorter.sort(temp, 1);
+
+            Assert.IsTrue(temp[0] == 1);
+
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            SetUp2();
+            int ultimo = 3;
+            //Bubble
+            temp = SortingAlgorithms.model.Sorter.sort(temp, 0);
+
+            Assert.IsTrue(temp[2] == ultimo);
+
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            SetUp2();
+            int ultimo = 3;
+            //Quick
+            temp = SortingAlgorithms.model.Sorter.sort(temp, 1);
+
+            Assert.IsTrue(temp[2] == ultimo);
+
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            SetUp3();
+            //Bubble
+            temp = SortingAlgorithms.model.Sorter.sort(temp, 0);
+
+            Assert.IsTrue(temp[0] == 1);
+
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            SetUp3();
+            //Quick
+            temp = SortingAlgorithms.model.Sorter.sort(temp, 1);
+
+            Assert.IsTrue(temp[0] == 1);
+
+        }
+
+        [TestMethod]
+        public void TestMethod11()
+        {
+            SetUp1();
+            int ultimo = 3;
+            //Bubble
+            temp = SortingAlgorithms.model.Sorter.sort(temp, 0);
+
+            Assert.IsTrue(temp[2] == ultimo);
+
+        }
+
+        [TestMethod]
+        public void TestMethod12()
+        {
+            SetUp1();
+            int ultimo = 3;
+            //Quick
+            temp = SortingAlgorithms.model.Sorter.sort(temp, 1);
+
+            Assert.IsTrue(temp[2] == ultimo);
 
         }
     }
