e9c0be1d1ebb327572db5cb70e9bee87cb0a0498
Danna García <dannasofiagarciatrujillo@hotmail.com>
2021-03-12 17:45:47 -0500

    UnitTest created again

---
+++ UnitTestProject/SorterTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using SortingAlgorithms;
+using SortingAlgorithms.model;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class SorterTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int[] temp = new int[3] { 1, 2, 3 };
+            temp = Sorter.sort(temp, 0);
+        }
+    }
+}
